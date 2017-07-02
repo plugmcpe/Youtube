68605130532ced7a71b280de11e65d1115ab0da8
plugmcpe <M7mmdgamer7@icloud.com>
2017-07-02 07:29:11 +0300

    Create Main.php

---
+++ src/Youtube/Main.php
@@ -0,0 +1,24 @@
+<?php
+namespace Youtube;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+class Main extends PluginBase{
+
+public function onEnable(){
+ $this->getServer()->getLogger()->info("plugin has been enable");
+  $this->getLogger()->info("by groupPluginer");
+   }
+   public function onDisable(){
+    $this->getServer()->getLogger()->info("plugin has been disable");
+    }
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+     switch ($cmd->getName()){
+      case 'Youtube':
+       $player = $sender->getPlayer();
+        $sender->setGamemode(1);
+        break;
+        }
+       }
+      }
